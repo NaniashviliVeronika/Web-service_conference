5a95b62efeef798cd2a4b96e554dc43e3c535d5e
neveronika <100508543+NaniashviliVeronika@users.noreply.github.com>
2023-07-12 15:44:48 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+let menuBtn = document.querySelector('.header__nav-burger');
+let menu = document.querySelector('.nav__item-lists');
+menuBtn.addEventListener('click', function(){
+    menu.classList.toggle('active');
+})
+
+
+let menuButton = document.querySelector('.header__nav-burger_1');
+let menu_1 = document.querySelector('.nav__item-lists_1');
+menuButton.addEventListener('click', function(){
+    menu_1.classList.toggle('active');
+})
+
+
+
+
+
+
